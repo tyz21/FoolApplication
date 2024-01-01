c83bbdbfea08c42e1226455cfdcc4fdfcecabfb8
tyz21 <tyzlife01@gmail.com>
2024-01-01 18:25:43 +0300

    update application.yaml

--- src/main/java/com/example/googleMapApplicationTracker/FoolApplication.java
+++ src/main/java/com/example/googleMapApplicationTracker/FoolApplication.java
@@ -3,7 +3,6 @@ package com.example.googleMapApplicationTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class FoolApplication {
 
